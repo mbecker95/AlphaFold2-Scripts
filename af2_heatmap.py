2b02ee25547c61d7a57cf5922ba07dce3040e105
mbecker95 <104383745+mbecker95@users.noreply.github.com>
2025-07-14 15:30:33 +0100

    Update to af2_heatmap.py

--- af2_heatmap.py
+++ af2_heatmap.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python
-# This script provides a way of outputting a heatmap of errors for AlphaFold models
-# 1.  Importing all of the appropriate modules for data
-#     interpretation and plotting
+#!/usr/bin/python
+"""
+File: af2_heatmap.py
+Author: Mark H. Becker
+Date: 2025-07-14
+Description: A Python script for outputting predicted aligned error heatmaps for AlphaFold models.
+"""
+
+# 1.  Importing all of the appropriate modules for data interpretation and plotting
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,16 +15,13 @@ import glob as glob
 import os
 import seaborn as sns
 
-# 2.  Obtaining the names of all of the pickle files and the
-#     directory of the outputs
+# 2.  Obtaining the names of all of the pickle files and the directory of the outputs
 
 files = glob.glob('result*.pkl', recursive=True)
 dir_name = os.getcwd().split('/')[-1]
 
-# 3a. Stripping the predicted alignment error, pTM and ipTM from
-#     the pickle files for each model.
-# 3b. Using the stripped data to plot the predicted alignement
-#     error while outputting the other parameters
+# 3a. Stripping the predicted alignment error, pTM and ipTM from the pickle files for each model.
+# 3b. Using the stripped data to plot the predicted alignment error while outputting the other parameters
 
 for file in files:
     df = pd.read_pickle(file) # add different models
